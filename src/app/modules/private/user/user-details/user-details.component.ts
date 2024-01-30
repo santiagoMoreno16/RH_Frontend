ac4e22e4ec0aa55c5c6c02e42d9e5b66932987fa
AnthonyLopezz <anthony.lopez@usantoto.edu.co>
2024-01-30 18:08:15 -0500

    Feat(User details): Pie Chart

--- src/app/modules/private/user/user-details/user-details.component.ts
+++ src/app/modules/private/user/user-details/user-details.component.ts
@@ -1,18 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Color } from '@swimlane/ngx-charts';
+import { Color, ScaleType } from '@swimlane/ngx-charts';
 import { ToastrService } from 'ngx-toastr';
-import { Subscription, catchError, map } from 'rxjs';
+import { Subscription } from 'rxjs';
 import { User } from 'src/app/models/user';
 import { UserService } from 'src/app/services/user.service';
-import { showMessage } from 'src/app/utils/messages/toast.func';
-import { observadorAny } from 'src/app/utils/observers/any-type';
 
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
   styleUrls: ['./user-details.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class UserDetailsComponent implements OnInit {
   view: [number, number] = [700, 400];
@@ -23,8 +22,11 @@ export class UserDetailsComponent implements OnInit {
   showLabels: boolean = true;
   isDoughnut: boolean = false;
 
-  colorScheme = {
-    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA'],
+  colorScheme: Color = {
+    domain: ['#0EA2A4', '#FABE16', '#E5470E', '#F4650F'],
+    name: 'colorScheme',
+    selectable: false,
+    group: ScaleType.Linear
   };
 
   private tmp: any;
@@ -32,18 +34,20 @@ export class UserDetailsComponent implements OnInit {
   public id: string = '';
   public cargaFinalizada: boolean;
   public sub: Subscription;
-  public oneName: string = ''; 
+  public oneName: string = '';
   constructor(
     public router: Router,
     public userService: UserService,
     public toastr: ToastrService,
-    private activatedRoute: ActivatedRoute,
-
+    private activatedRoute: ActivatedRoute
   ) {
     this.sub = this.tmp;
     this.cargaFinalizada = false;
   }
 
+  get single() {
+    return this.userService.points;
+  }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe((params) => {
@@ -62,32 +66,16 @@ export class UserDetailsComponent implements OnInit {
   }
 
   private getOneName(): void {
-    if (this.userModel?.user?.firstName && this.userModel.user.firstName.includes(' ')) {
+    if (
+      this.userModel?.user?.firstName &&
+      this.userModel.user.firstName.includes(' ')
+    ) {
       const nombres = this.userModel.user.firstName.split(' ');
       this.oneName = nombres[0];
     } else {
       this.oneName = this.userModel?.user?.firstName || '';
     }
   }
-
-  single = [
-    {
-      name: 'Germany',
-      value: 8940000,
-    },
-    {
-      name: 'USA',
-      value: 5000000,
-    },
-    {
-      name: 'France',
-      value: 7200000,
-    },
-    {
-      name: 'UK',
-      value: 6200000,
-    },
-  ];
 
   onSelect(data: any): void {
     console.log('Item clicked', JSON.parse(JSON.stringify(data)));
@@ -100,5 +88,4 @@ export class UserDetailsComponent implements OnInit {
   onDeactivate(data: any): void {
     console.log('Deactivate', JSON.parse(JSON.stringify(data)));
   }
-
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -5,14 +5,42 @@ import { User } from '../models/user';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 
+interface Points {
+  name: string;
+  value: number;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
+  private data: Points[] = [
+    {
+      name: 'Puntos Estratégicos',
+      value: 500,
+    },
+    {
+      name: 'Puntos Tácticos',
+      value: 1500,
+    },
+    {
+      name: 'Puntos Operacionales',
+      value: 2000,
+    },
+    {
+      name: 'Puntos Personales',
+      value: 1000,
+    },
+  ];
+
   public apiGetUser: string = serverUrl.API_GET_USER;
 
   constructor(private http: HttpClient) {}
   public getUser(id: string): Observable<User> {
     return this.http.get<User>(`${this.apiGetUser}/${id}`);
+  }
+
+  get points() {
+    return this.data;
   }
 }
